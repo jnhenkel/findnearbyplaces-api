4a03dcdea46b538a1062d0ca14d5bfebc21993da
jnhenkel <jeffnhenkel@gmail.com>
2022-04-10 10:33:52 -0700

    create get root path and connect to heroku

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const app = express();
 
 const port = process.env.PORT || 4002;
 
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req,res) => {
+    res.status(200).json({done: true, message: 'This is the backend for findnearbyplaces'});
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
