404314aa2f806eb2c89438c4293c212000d879f3
jnhenkel <jeffnhenkel@gmail.com>
2022-04-22 08:53:34 -0700

    get search started

---
+++ data/store.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcrypt');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const connectionString = `postgres://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.DATABASEPORT}/${process.env.DATABASE}`;
+
+const connection = {
+    connectionString: process.env.DATABASE_URL ? process.env.DATABASE_URL : connectionString,
+    ssl: { rejectUnauthorized: false }
+}
+
+const pool = new Pool(connection);
+
+let store = {
+    getSearch: (search_term, user_location, radius_filter, maximum_results_to_return, category_filter, sort) => {
+        
+    }
+}
+
+modeule.exports = {store};
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var cors = require('cors');
+import store from './data/store';
 
 const app = express();
 
@@ -14,6 +15,15 @@ app.get('/', (req,res) => {
     res.status(200).json({done: true, message: 'This is the backend for findnearbyplaces 1'});
 });
  
+app.get('/search/:search_term/:user_location/:radius_filter/:maximum_results_to_return/:category_filter/:sort', (req, res) => {
+    let search_term = req.params.search_term;
+    let user_location = req.params.user_location;
+    let radius_filter = req.params.radius_filter;
+    let maximum_results_to_return = req.params.maximum_results_to_return;
+    let category_filter = req.params.category_filter;
+    let sort = req.params.sort;
+    store.getSearch(search_term, user_location, radius_filter, maximum_results_to_return, category_filter, sort)
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
